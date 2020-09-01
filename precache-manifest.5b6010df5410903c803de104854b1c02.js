e78cbd7f48f44db51c382f95a42f66f520768148
Jonathan Alphono <jono@Jonathans-MacBook-Pro.local>
2020-09-01 16:45:21 +1200

    Updates

---
+++ precache-manifest.5b6010df5410903c803de104854b1c02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea24a2b32ff676fb7bdde0099d9b6928",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "abe55e781109117d305c",
+    "url": "/frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "901583ee10f65197a134",
+    "url": "/frontend/static/js/2.c24a221a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/frontend/static/js/2.c24a221a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe55e781109117d305c",
+    "url": "/frontend/static/js/main.ba71ba0e.chunk.js"
+  },
+  {
+    "revision": "747c638a0ed8091beb0a",
+    "url": "/frontend/static/js/runtime-main.12e52466.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
