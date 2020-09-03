a1b967b31ac85931989a7fb0770118fe9912450a
TheLastPiixel <48301981+TheLastPiixel@users.noreply.github.com>
2020-09-03 23:00:35 +1200

    Removed Google Maps API Key

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
     <div>
     <div style={{ width: "100vw", height: "50vh"}}>
       <WrappedMap 
-      googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}"}
+      googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key="}
       loadingElement={<div style={{ height: "100%" }}/>}
       containerElement={<div style={{ height: "100%" }}/>}
       mapElement={<div style={{ height: "100%" }}/>}
