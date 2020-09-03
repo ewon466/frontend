033ac2f2baa09ab0690afc9a052bd456a97d664a
TheLastPiixel <48301981+TheLastPiixel@users.noreply.github.com>
2020-09-03 23:34:30 +1200

    Jono made me do this on the git console....

--- src/App.js
+++ src/App.js
@@ -17,11 +17,12 @@ function Map() {
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 function App() {
+  console.log(process.env.REACT_APP_GOOGLE_KEY);
   return (
     <div>
     <div style={{ width: "100vw", height: "50vh"}}>
       <WrappedMap 
-      googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key="}
+      googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
       loadingElement={<div style={{ height: "100%" }}/>}
       containerElement={<div style={{ height: "100%" }}/>}
       mapElement={<div style={{ height: "100%" }}/>}
