0edc89acf0030a5d526997e89e2d86afcbfa597a
ewon466 <48311905+ewon466@users.noreply.github.com>
2020-09-06 18:04:32 +1200

    Update App.js
    take lat/lng and renders marker on map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
-import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, } from "react-google-maps";
 import './App.css';
+
 
 require("dotenv").config();
 
 function Map() {
   return (
       <GoogleMap
+      onClick = {(event) =>{
+        console.log(event);
+      }}
         defaultZoom={10}
         defaultCenter={{ lat: -36.848461, lng: 174.763336 }} //auckland city co-ordinates
-      />
+       
+      >
+      
+      <Marker  
+      position={{lat:-36,lng:174}}/>)
+      </GoogleMap>
+     
   );
 }
 
@@ -21,7 +31,7 @@ function App() {
     <div>
     <div style={{ width: "100vw", height: "50vh"}}>
       <WrappedMap 
-      googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key="}
+      googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCWKV_oO0Jo8ih5KMrT4H0Eyvf4cPBNVIU"}
       loadingElement={<div style={{ height: "100%" }}/>}
       containerElement={<div style={{ height: "100%" }}/>}
       mapElement={<div style={{ height: "100%" }}/>}
