ef978b7b792ac3c00821c3edec309894e909cc5f
brianzhangmz <bzha841@aucklanduni.ac.nz>
2020-09-24 03:05:58 +1200

    Basic UI implemented for displaying user info

--- src/CustomerInfo/CustomerInfo.js
+++ src/CustomerInfo/CustomerInfo.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import "./CustomerInfo.css";
 import axios from "axios";
+import { Table, Button } from "react-bootstrap";
 
 class CustomerInfo extends Component {
   // eslint-disable-next-line
   constructor(props) {
     super(props);
+
+    this.state = {
+      isOpen: false,
+      error: null,
+      users: [],
+      userData: {},
+      response: {},
+    };
   }
 
   componentWillMount() {
@@ -24,7 +33,39 @@ class CustomerInfo extends Component {
   }
 
   render() {
-    return <div>response</div>;
+    return (
+      <div className="App">
+        <Table className="table">
+          <thead>
+            <tr className="btn-primary">
+              <th colSpan="2">User Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>First Name </th>
+              <td>Kobe</td>
+            </tr>{" "}
+            <tr>
+              <th>Last Name </th>
+              <td>Byrant</td>
+            </tr>{" "}
+            <tr>
+              <th>Mobile No </th>
+              <td>6422222222</td>
+            </tr>{" "}
+            <tr>
+              <th>Address </th>
+              <td>Auckland</td>
+            </tr>{" "}
+            <tr>
+              <th>Order </th>
+              <td>kfc</td>
+            </tr>
+          </tbody>
+        </Table>
+      </div>
+    );
   }
 }
 
